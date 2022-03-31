73f0a82a21f1383c85b6c340404f19809b7c959a
robbyrao <58942695+robbyrao@users.noreply.github.com>
2022-03-31 17:42:00 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,3 +7,5 @@
 #=================================================
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+echo 'src-git liuran001_packages https://github.com/liuran001/openwrt-packages' >>feeds.conf.default
